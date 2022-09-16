e6037257b73f2c52d30f4a1b08c761643de7819e
itskayX404 <81577355+itskayX404@users.noreply.github.com>
2022-09-17 05:46:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['6282180026483']
+global.premium = ['6282180026483']
+global.packname = 'Havy-Bot'
+global.author = 'itsmekay'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
